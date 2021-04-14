1bd08cce6d748f7a179198b7f1c83d62b2fc7db3
Longhao.Chen <Longhao.Chen@outlook.com>
2021-04-14 13:46:43 +0800

    add:复数运算
    新文件：   接收报警模块/complex.cpp

---
+++ 接收报警模块/complex.cpp
@@ -0,0 +1,40 @@
+//复数运算
+#include "dsp.hpp"
+
+//复数乘法
+template <typename T>
+complex<T> mul(complx<T> &a, complex<T> &b)
+{
+	complex<T> res;
+	res.re = a.re * b.re - a.im * b.im;
+	res.im = a.re * b.im + a.im * b.re;
+	return res;
+}
+
+//加法
+template <typename T>
+complex<T> add(complex<T> &a, complex<T> &b)
+{
+	complex<T> res;
+	res.re = a.re + b.re;
+	res.im = a.im + b.im;
+	return res;
+}
+
+//减法
+template <typename T>
+complex<T> add(complex<T> &a, complex<T> &b)
+{
+	complex<T> res;
+	res.re = a.re - b.re;
+	res.im = a.im - b.im;
+}
+
+//共轭
+template <typename T>
+complex<T> conj(complex<T> &a){
+	complex res;
+	res.re = a.re;
+	res.im = -a.im;
+	return res;
+}
