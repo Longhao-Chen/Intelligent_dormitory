566d241175c5031208387cc6366be67911091e9f
Chen Longhao <chenlhlinux@outlook.com>
2021-04-21 16:01:22 +0800

    feat:为方差计算和平均值计算添加跳过关键频率的支持

--- 接收报警模块/statistics.hpp
+++ 接收报警模块/statistics.hpp
@@ -3,7 +3,7 @@ Copyright (c) 2021 Longhao-Chen. All rights reserved.
 */
 
 template <typename T, size_t N>
-float var(const T (&data)[N], float avg);
+float var(const T (&data)[N], float avg, int skip);
 
 template <typename T, size_t N>
-float avg(const T (&data)[N]);
+float avg(const T (&data)[N], int skip);
